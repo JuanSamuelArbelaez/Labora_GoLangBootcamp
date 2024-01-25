0bab35a8ef877c42d9e5a2c0685e57cf7feccd1e
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-01-25 15:17:32 -0500

    Functions and Loops implemented in Planet program
    Planet Mapping finished

--- Seccion-1/planets.go
+++ Seccion-1/planets.go
@@ -2,32 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	var planetsMoons = map[string]int{
-		"Mercury":0,
-		"Venus":0,
-		"Earth":1,
-		"Mars":2,
-		"Jupiter":95,
-		"Saturn":146,
-		"Uranus":27,
-		"Neptune":14,
-	}
-
-	printPlanet(planetsMoons, "Mercury")
-	printPlanet(planetsMoons, "Venus")
-	printPlanet(planetsMoons, "Earth")
-	printPlanet(planetsMoons, "Mars")
-	printPlanet(planetsMoons, "Jupiter")
-	printPlanet(planetsMoons, "Saturn")
-	printPlanet(planetsMoons, "Uranus")
-	printPlanet(planetsMoons, "Neptune")
+var planets= map[string]int{
+	"Mercury":0,
+	"Venus":0,
+	"Earth":1,
+	"Mars":2,
+	"Jupiter":95,
+	"Saturn":146,
+	"Uranus":27,
+	"Neptune":14,
 }
 
-func printPlanet(planets map[string]int, planetName string){
+func main() {
+	printAllPlanets()
+}
+
+func printPlanet(planetName string){
 	if numberOfMoons, ok := planets[planetName]; ok {
 		fmt.Println(planetName, ":", numberOfMoons)
 	} else {
 		panic("No planet with name <" + planetName + ">")
+	}
+}
+
+func printAllPlanets(){
+	for planetName := range planets{
+		printPlanet(planetName)
 	}
 }
