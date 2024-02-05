8d0235073f3687cf6b7845d178a61b59ac5ad302
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-02-05 17:16:23 -0500

    Adición de IMC y categoría respectiva a los datos mostrados de las personas

--- Seccion-1/Prácticas-2/personas.go
+++ Seccion-1/Prácticas-2/personas.go
@@ -19,10 +19,21 @@ func calcularIMC(persona Persona) float64 {
 	return persona.Peso / math.Pow(persona.Altura, 2)
 }
 
-// Función para mostrar las personas
+// Función para mostrar las personas con su IMC y categoría
 func mostrarPersonas(personas []Persona) {
 	for _, p := range personas {
-		fmt.Printf("Nombre: %s, Edad: %d, Altura: %.2f, Peso: %.2f ", p.Nombre, p.Edad, p.Altura, p.Peso)
+		imc := calcularIMC(p)
+		fmt.Printf("Nombre: %s, Edad: %d, Altura: %.2f, Peso: %.2f, IMC: %.2f - ", p.Nombre, p.Edad, p.Altura, p.Peso, imc)
+		switch {
+		case imc < 18.5:
+			fmt.Println("Bajo peso")
+		case imc >= 18.5 && imc <= 24.9:
+			fmt.Println("Peso normal")
+		case imc >= 25 && imc <= 29.9:
+			fmt.Println("Sobrepeso")
+		default:
+			fmt.Println("Obesidad")
+		}
 	}
 }
 
