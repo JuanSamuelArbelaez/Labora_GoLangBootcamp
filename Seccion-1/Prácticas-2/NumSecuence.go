6cc225bac12376f114b1ca8330b43667dc84e512
JuanSamuelArbelaez <109239223+JuanSamuelArbelaez@users.noreply.github.com>
2024-02-23 19:18:36 -0500

    Implementacion de obtención del siguiente primo y numero en secuencia.

--- Seccion-1/Prácticas-2/NumSecuence.go
+++ Seccion-1/Prácticas-2/NumSecuence.go
@@ -19,6 +19,51 @@ type PrimeNumbers struct {
 	numbers []int
 }
 
+func (inc *IncreasingNumbers) Next() int {
+	nextNumber := len(inc.numbers) + 1
+	inc.numbers = append(inc.numbers, nextNumber)
+	return nextNumber
+}
+
+func (prime *PrimeNumbers) Next() int {
+	if prime.numbers == nil {
+		prime.numbers = append(prime.numbers, 2) // Start with 2, the first prime
+		return 2
+	}
+
+	lastNumber := prime.numbers[len(prime.numbers)-1]
+	newNumber := lastNumber + 1
+	for {
+		if isPrime(newNumber) {
+			prime.numbers = append(prime.numbers, newNumber)
+			break
+		}
+		newNumber++
+	}
+	return newNumber
+}
+
+func (inc *IncreasingNumbers) First30() []int {
+	for len(inc.numbers) < 30 {
+		inc.Next()
+	}
+	return inc.numbers
+}
+
+func (prime *PrimeNumbers) First30() []int {
+	for len(prime.numbers) < 30 {
+		prime.Next()
+	}
+	return prime.numbers
+}
+
+func (inc *IncreasingNumbers) Title() {
+	fmt.Println("Sequence of Increasing Numbers")
+}
+
+func (prime *PrimeNumbers) Title() {
+	fmt.Println("Sequence of Prime Numbers")
+}
 
 func isPrime(n int) bool {
 	if n <= 1 {
@@ -40,6 +85,7 @@ func printArray(arr []int) {
 			fmt.Print(", ")
 		}
 	}
+	fmt.Println("]")
 }
 
 func main() {
